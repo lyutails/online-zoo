da5d53cc6e19c8f888dd809f57fdabec02a0d722
lyutails <lyusya.cool@gmail.com>
2022-09-26 20:01:39 +0300

    fix: correct placement in footer

---
+++ online-zoo/page_main/script.js
@@ -0,0 +1,7 @@
+const element = document.querySelector('.submit_button');
+
+function submitDefault (object) {
+    if ((object.placeholder.match('Enter your email')))
+    element.style.color = '#404950';
+    element.style.border = '#404950';
+}
